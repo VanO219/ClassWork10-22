6ea149c0b6635343c5c87fa545c2d041976b10bb
Ivan Sinyavsky <ivan.s@dev.izibook.ru>
2021-03-29 11:30:21 +0300

    задачи на логику и алгоритмы

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module ClassWork
 
 go 1.16
-
-require (
-	github.com/lib/pq v1.10.0
-	github.com/pkg/errors v0.9.1
-)
--- main.go
+++ main.go
@@ -1,65 +1,7 @@
 package main
 
-import (
-	"database/sql"
-	"fmt"
-	"log"
-	"net/url"
-
-	_ "github.com/lib/pq"
-	"github.com/pkg/errors"
-)
-
-type Conn struct {
-	ConnTest *sql.DB
-}
-
-func (db *Conn) Connect(user string, password string, dbname string, port int, host string) (err error) {
-	defer func() { err = errors.Wrap(err, "Conn.Connect") }()
-	//connStr := "postgres://pqgotest:password@localhost/pqgotest?sslmode=verify-full"
-	u := url.URL{
-		Scheme: "postgres",
-		Host:   fmt.Sprintf("%s:%d", host, port),
-		Path:   "test",
-	}
-	// v := url.Values{}
-	v, err := url.ParseQuery("sslmode=disable")
-	if err != nil {
-		err = errors.Wrap(err, "Failed ParseQuery")
-		return
-	}
-	u.RawQuery = v.Encode()
-	u.User = url.UserPassword(user, password)
-	// connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable port=%d", user, password, dbname, port)
-	fmt.Println(u.String())
-	db.ConnTest, err = sql.Open("postgres", u.String())
-	if err != nil {
-		err = errors.Wrap(err, "Failed connection to DB")
-		return
-	}
-	return
-}
-
-func (db *Conn) Close() (err error) {
-	defer func() { err = errors.Wrap(err, "Conn.Close") }()
-	err = db.ConnTest.Close()
-	if err != nil {
-		err = errors.Wrap(err, "Failed close DB")
-		return
-	}
-	return
-}
+import "fmt"
 
 func main() {
-	var conTest Conn
-	err := conTest.Connect("ivan_user", "password", "test", 5432, "localhost")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer func() {
-		err = conTest.Close()
-		if err != nil {
-			log.Fatalln(err)
-		}
-	}()
+	fmt.Println("hello")
 }
