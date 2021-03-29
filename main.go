31e5051d905cbfb0906f14b9e394bfe0b19f5c87
Ivan Sinyavsky <ivan.s@dev.izibook.ru>
2021-03-29 12:54:27 +0300

    Разбить число на разряды (Сергей)

--- main.go
+++ main.go
@@ -7,34 +7,52 @@ import (
 )
 
 func main() {
-	fmt.Println(Cascades(23000789.01))
+	fmt.Println(Cascades(23000789.00))
 }
-func Cascades(num float64) (nm string) {
-	str := strconv.FormatFloat(num, 'f', -1, 64)
-	
-	pointStr := strings.Split(str, ".")
-	answer := ""
-	to := len(pointStr[0])
-	step := 3
-	for {
-		if (to - step) > 0 {
-			answer = pointStr[0][to-step:to] + " " + answer
-			to = to - step
-		} else {
-			answer = pointStr[0][0:to] + " " + answer
-			break
-		}
+func Cascades(input float64) (result string) {
+	var st []string
+	var strlen int
+	var output []string
 
-	}
-	lenAns := len(answer)
-	answer = answer[:lenAns-1]
-	answer = answer + "." + pointStr[1]
-	for {
-		if answer[0] == '0' {
-			answer = answer[1:lenAns]
-		} else {
-			break
+	ghf := (int)(input * 100)
+	s := fmt.Sprintf("%f", input)
+	st = strings.Split(s, ".")
+	strlen = len([]rune(st[0]))
+
+	if strlen < 17 {
+		if (strlen % 3) == 0 {
+			output = strings.SplitAfterN(st[0], "", strlen)
+			s1 := strings.Join([]string{output[0], output[1], output[2]}, "")
+			result = s1
+			for i := 3; i < strlen; i += 3 {
+				s1 = strings.Join([]string{output[i], output[i+1], output[i+2]}, "")
+				result = result + " " + s1
+			}
+		} else if (strlen % 3) == 2 {
+			output = strings.SplitAfterN(st[0], "", strlen)
+			s1 := strings.Join([]string{output[0], output[1]}, "")
+			result = s1
+			for i := 2; i < strlen; i += 3 {
+				s1 = strings.Join([]string{output[i], output[i+1], output[i+2]}, "")
+				result = result + " " + s1
+			}
+		} else if (strlen % 3) == 1 {
+			output = strings.SplitAfterN(st[0], "", strlen)
+			s1 := strings.Join([]string{output[0]}, "")
+			result = s1
+			for i := 1; i < strlen; i += 3 {
+				s1 = strings.Join([]string{output[i], output[i+1], output[i+2]}, "")
+				result = result + " " + s1
+			}
 		}
+		if g := ghf % 100; g != 0 {
+			result += ", " + strconv.Itoa(g)
+			return result
+		} else {
+			result += ",00"
+			return result
+		}
+	} else {
+		return ("Много букОв")
 	}
-	return answer
 }
